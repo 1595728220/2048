7a6c5bc4c0f8f6dacf0f2301c66d08a17aa94757
1595728220 <1595728220@qq.com>
2019-07-05 22:23:29 +0800

    主体布局中

---
+++ src/views/game.vue
@@ -0,0 +1,30 @@
+<template>
+  <div class="pan">
+      <my-square :num="num"></my-square>
+  </div>
+</template>
+<script>
+import Square from "../components/square";
+export default {
+  data() {
+    return {
+      num
+    };
+  },
+  components: {
+    "my-square": Square
+  }
+};
+</script>
+<style lang="scss" scoped>
+    .pan{
+        width:500px;
+        height:500px;
+        border-radius: 10px;
+        background:#BBADA0;
+        position: absolute;
+        top:50%;
+        left:50%;
+        transform: translate(-50%,-50%)
+    }
+</style>
